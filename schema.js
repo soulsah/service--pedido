654cf0633638aaa97b67586f62351df2c2c32e67
Marcos Fernandes Alves <marcosfernandesalves99@hotmail.com>
2024-08-25 16:34:17 -0300

    subindo projeto

--- schema.js
+++ schema.js
@@ -3,8 +3,8 @@ db = db.getSiblingDB('pedidos');
 db.createCollection("pedidos", { capped: false });
 
 var pedido1 = db.pedidos.insertOne({
-    "pedidoId": "",
-    "clienteId": "",
+    "pedidoId": "08f756c4-d5ce-4ef8-90d4-76837f39c42c",
+    "clienteId": "2",
     "produtoId": [
         { "id": 122, "quantidade": 2 }
     ],
